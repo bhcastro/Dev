8e91fbaa954a660c3f80b60cc518e8e23f2f7126
Bruno Castro <bruno.hen.castro@gmail.com>
2025-02-03 17:40:52 -0300

    Atualizações
    Atualizações e demais ajustes de código e layouts

--- Projetos/Landing Page/js/scripts_calculadoras/calc_assid.js
+++ Projetos/Landing Page/js/scripts_calculadoras/calc_assid.js
@@ -1,29 +1,31 @@
 const inputPlantoes = document.getElementById('input__plantoes');
 const inputFaltas = document.getElementById('input__faltas');
 const inputAtrasos = document.getElementById('input__atrasos');
-const inputAtestados = document.getElementById('input__atestados');
 const outputAssiduidade = document.getElementById('output__assiduidade');
 const buttonCalcularAssid = document.getElementById('botao__calc__assid');
 const buttonZerarCampos = document.getElementById('botao__limpar__campos')
 const paragrafoAssiduidade = document.getElementById('paragrafo__assiduidade');
+const diasZerado = 'Insira um valor válido para os dias trabalhados!';
+const diasNegativo = 'Não é permitido inserir números negativos';
+
 
 function calcularAssiduidade(){
     let diasTrabalhados = parseInt(inputPlantoes.value);
     let faltasNaoJustificadas = parseInt(inputFaltas.value) || 0;
-    let totalAtestados = parseInt(inputAtestados.value) || 0;
     let atrasos = parseInt(inputAtrasos.value) || 0;
-    if(!isNaN(diasTrabalhados) && !isNaN(atrasos)){
+    if(!isNaN(diasTrabalhados) && diasTrabalhados >= 0 && faltasNaoJustificadas >= 0 && faltasNaoJustificadas >= 0 && !isNaN(atrasos) && atrasos >= 0){
     faltasNaoJustificadas = faltasNaoJustificadas*2;
-    totalAtestados = totalAtestados * 0.5;
-    atrasos = atrasos + faltasNaoJustificadas + totalAtestados;
+    atrasos = atrasos + faltasNaoJustificadas;
     let totalAtrasos = (atrasos/diasTrabalhados)*100;
     let totalAssiduidade = 100 - totalAtrasos;
     totalAssiduidade = totalAssiduidade.toFixed(2); 
     outputAssiduidade.innerText = `${totalAssiduidade}%`;
     paragrafoAssiduidade.classList.remove('ocultar');
     buttonZerarCampos.classList.remove('ocultar');
+    }else if(diasTrabalhados < 0 || atrasos < 0 || faltasNaoJustificadas < 0){
+    alert(diasNegativo);
     }else{
-    alert('A quantidade de dias trabalhados é obrigatória!');
+        alert(diasZerado);
     }
 }
 
@@ -31,7 +33,6 @@ function limparCampos(){
     inputPlantoes.value = "";
     inputFaltas.value = "";
     inputAtrasos.value = "";
-    inputAtestados.value = "";
     outputAssiduidade.innerText = "";
     paragrafoAssiduidade.classList.toggle('ocultar');
     buttonZerarCampos.classList.toggle('ocultar');
