9a4e19fa0d446ddc6018f0c86676e2de69dedad3
Bruno Castro <bruno.hen.castro@gmail.com>
2025-07-23 17:19:47 -0300

    inicio da configuração da opções

--- Projetos/Lab/interanalista/scripts/observacoes.js
+++ Projetos/Lab/interanalista/scripts/observacoes.js
@@ -1,49 +1,52 @@
+const listaObsUrina = document.getElementById('lista-obs-urina');
+const botaoAdicionarUrina = document.getElementById('botao-add-obs-urina');
+let inputObsUrina = document.getElementById('input-obs-urina');
+
+const botaoAdicionarHemato = document.getElementById('botao-add-obs-hemato');
+let inputObsHemato = document.getElementById('input-obs-hemato');
+
+const valorInvalido = 'Não é possível inserir observções em branco.';
+const valorRepetido = 'Observação já inserida';
+
 let arrayObsHemato = [];
 let arrayObsUrina = [];
-const botaoAdicionarHemato = document.getElementById("add-obs-hemato");
-const botaoAdicionarUrina = document.getElementById("add-obs-urina");
 
-function pegarObsHemato(){
-  let inputObsHemato = document.getElementById('input-obs-hemato');
-  let obsHemato = inputObsHemato.value.trim();
-  arrayObsHemato.push(obsHemato);
-  let listaHemato = document.getElementById("lista-obs-hemato");
-  let novaObs = document.createElement('li');
-  novaObs.textContent = obsHemato;
-  listaHemato.appendChild(novaObs);
-  inputObsHemato.value = "";
-  inputObsHemato.focus();
-}
-
-function pegarObsUrina(){
-  let inputObsUrina = document.getElementById('input-obs-urina');
-  let obsUrina = inputObsUrina.value.trim();
-  arrayObsUrina.push(obsUrina);
-  let listaUrina = document.getElementById('lista-obs-urina');
-  let novaObs = document.createElement('li');
-  novaObs.textContent = obsUrina;
-  listaUrina.appendChild(novaObs);
-  //atualizarListas(listaUrina, arrayObsUrina);
-  inputObsUrina.value = "";
-  inputObsUrina.focus();
-}
-
-
-
-function atualizarListas(setor, array){
-  setor.textContent = "";
-  let i = 0;
-  for (i; i<array.length; i++){
-    let novaObs = document.createElement("li");
-    novaObs.innerHTML = array[i];
-    setor.appendChild(novaObs);
+function pegarValor(array, input){
+  let novaObs = input.value.trim();
+  if(novaObs === ""){
+    alert(valorInvalido);
+    input.focus();
+  }else if(array.includes(novaObs)){
+    alert(valorRepetido);
+    input.value = "";
+    input.focus();
+  }else{
+    array.push(novaObs);
+    alert(array, novaObs);
+    input.value = "";
+    input.focus();
+    mostrarObs(array, i)
   }
 }
 
-function removerObs(i, setor, array){
-  array.splice(i, 1);
-  atualizarListas(setor, array);  
+function mostrarObs(array, lista, i){
+  array.innerHTML = "";
+  for(i = 0; i <array.length; i++){
+    let novaObs = document.createElement('li');
+    let botaoEditar = document.createElement('button');
+    let botaoRemover = document.createElement('button');
+
+    novaObs.textContent = i;
+    botaoEditar.textContent = 'Editar';
+    botaoRemover.textContent = 'Remover';
+
+    novaObs.appendChild(botaoEditar);
+    novaObs.appendChild(botaoRemover);
+    lista.appendChild(novaObs);
+
+
+  }
 }
 
-botaoAdicionarHemato.addEventListener("click", pegarObsHemato);
-botaoAdicionarUrina.addEventListener("click", pegarObsUrina);
+botaoAdicionarUrina.addEventListener('click', ()=> pegarValor(arrayObsUrina, inputObsUrina));
+botaoAdicionarHemato.addEventListener('click', ()=> pegarValor(arrayObsHemato, inputObsHemato));
